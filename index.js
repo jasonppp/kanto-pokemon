b640bad90b5c38b1cd0c297573c20718d0fad5db
Jason Y. Huang <Jason.Y.Huang@kp.org>
2023-05-09 23:42:25 -0700

    Fixed pokeImage URL location

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 console.log('You have connected...')
 
-document.addEventListener("DOMContentLoaded", () =>{
+document.addEventListener("DOMContentLoaded", () => {
 
     let generateBtn = document.querySelector('#generate-pokemon');
     generateBtn.addEventListener('click', renderEverything)
@@ -8,7 +8,7 @@ document.addEventListener("DOMContentLoaded", () =>{
     getDeleteBtn().addEventListener('click', deleteEverything);
 })
 
-function renderEverything(){
+function renderEverything() {
     let allPokemonContainer = document.querySelector('#poke-container')
     allPokemonContainer.innerText = "";
     fetchKantoPokemon();
@@ -16,47 +16,47 @@ function renderEverything(){
     getDeleteBtn().style.display = 'block'
 }
 
-function getDeleteBtn(){
+function getDeleteBtn() {
     return document.querySelector('#delete-btn')
 }
 
 
-function fetchKantoPokemon(){
+function fetchKantoPokemon() {
     fetch('https://pokeapi.co/api/v2/pokemon?limit=151')
-    .then(response => response.json())
-    .then(function(allpokemon){
-        allpokemon.results.forEach(function(pokemon){
-            fetchPokemonData(pokemon);
+        .then(response => response.json())
+        .then(function (allpokemon) {
+            allpokemon.results.forEach(function (pokemon) {
+                fetchPokemonData(pokemon);
+            })
         })
-    })
 }
 
-function fetchPokemonData(pokemon){
+function fetchPokemonData(pokemon) {
     let url = pokemon.url // <--- this is saving the pokemon url to a variable to use in the fetch. 
-                                //Example: https://pokeapi.co/api/v2/pokemon/1/"
+    //Example: https://pokeapi.co/api/v2/pokemon/1/"
     fetch(url)
-    .then(response => response.json())
-    .then(function(pokeData){
-        renderPokemon(pokeData)
-    })
+        .then(response => response.json())
+        .then(function (pokeData) {
+            renderPokemon(pokeData)
+        })
 }
 
 
-function renderPokemon(pokeData){
+function renderPokemon(pokeData) {
     let allPokemonContainer = document.getElementById('poke-container');
     let pokeContainer = document.createElement("div") //div will be used to hold the data/details for indiviual pokemon.{}
     pokeContainer.classList.add('ui', 'card');
 
     createPokeImage(pokeData.id, pokeContainer);
 
-    let pokeName = document.createElement('h4') 
+    let pokeName = document.createElement('h4')
     pokeName.innerText = pokeData.name
 
     let pokeNumber = document.createElement('p')
     pokeNumber.innerText = `#${pokeData.id}`
-   
+
     let pokeTypes = document.createElement('ul') //ul list will hold the pokemon types
-  
+
 
     createTypes(pokeData.types, pokeTypes) // helper function to go through the types array and create li tags for each one
 
@@ -64,26 +64,26 @@ function renderPokemon(pokeData){
     allPokemonContainer.appendChild(pokeContainer);       //appending that pokeContainer div to the main div which will                                                             hold all the pokemon cards
 }
 
-function createTypes(types, ul){
-    types.forEach(function(type){
+function createTypes(types, ul) {
+    types.forEach(function (type) {
         let typeLi = document.createElement('li');
         typeLi.innerText = type['type']['name'];
         ul.append(typeLi)
     })
 }
 
-function createPokeImage(pokeID, containerDiv){
+function createPokeImage(pokeID, containerDiv) {
     let pokeImgContainer = document.createElement('div')
     pokeImgContainer.classList.add('image')
 
     let pokeImage = document.createElement('img')
-    pokeImage.srcset = `https://pokeres.bastionbot.org/images/pokemon/${pokeID}.png`
+    pokeImage.srcset = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${pokeID}.png`
 
     pokeImgContainer.append(pokeImage);
     containerDiv.append(pokeImgContainer);
 }
 
-function deleteEverything(event){
+function deleteEverything(event) {
     event.target.style = 'none';
     let allPokemonContainer = document.querySelector('#poke-container')
     allPokemonContainer.innerText = ""
